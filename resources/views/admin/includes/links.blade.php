45823fa6fa695db4a0c7476ca2b4079a2b76260b
V Care Technologies <info@vcaretechnologies.net>
2024-08-30 14:59:28 +0530

    changes

--- resources/views/admin/includes/links.blade.php
+++ resources/views/admin/includes/links.blade.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="{{ url('public/admin/css/style.css') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="{{ url('public/admin/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ url('public/admin/css/metisMenu.min.css') }}">
 <link rel="stylesheet" href="{{ url('public/admin/css/toastr.min.css') }}">
 <link rel="stylesheet" href="{{ url('public/admin/css/select2.min.css') }}">
 <link rel="stylesheet" href="{{ url('public/admin/css/select2-bootstrap.css') }}">
 <link rel="stylesheet" href="{{ url('public/admin/css/multi-select.css') }}">
-<link rel="stylesheet" href="{{ url('public/admin/css/sweetalert2.min.css') }}">
-<link rel="stylesheet" href="https://tarwalas.in/myts/public/admin/css/metisMenu.min.css"> 
+<link rel="stylesheet" href="{{ url('public/admin/css/sweetalert2.min.css') }}"> 
